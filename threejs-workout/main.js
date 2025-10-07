0832806c1cd81b92117a14835a3cdea474427732
markphd <markphd@pm.me>
2025-10-07 23:58:45 +1100

    add fov formula

--- threejs-workout/main.js
+++ threejs-workout/main.js
@@ -26,3 +26,25 @@ const line = new THREE.Line( geometry, material );
 
 scene.add(line);
 renderer.render(scene, camera);
+
+// remember these initial values
+var tanFOV = Math.tan(((Math.PI / 180) * camera.fov / 2));
+var windowHeight = window.innerHeight;
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize( event ) {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    
+    // adjust the FOV
+    camera.fov = ( 360 / Math.PI ) * Math.atan( tanFOV * ( window.innerHeight / windowHeight ) );
+    
+    camera.updateProjectionMatrix();
+    camera.lookAt( scene.position );
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.render( scene, camera );
+    
+}
+
